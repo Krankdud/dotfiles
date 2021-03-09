26834af2d934ebe96adfd555ed8f0a7b5cc8cb0c
charlie <charlieschuy@gmail.com>
2021-03-09 02:16:55 -0800

    [fish/fzf] fo alias to open in vim

--- config.fish
+++ config.fish
@@ -1,5 +1,8 @@
 alias vim="nvim"
 alias python="python3"
+alias bath="ssh bath@50.116.20.111"
+
+alias fo='vim (fzf --preview="bat --theme=base16 --color=always --style=plain {}")'
 
 set FZF_DEFAULT_COMMAND "fd --type f"
 
